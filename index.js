fa19ae078e566c219eeae945b604c09f16a2df3b
Sara Nooraldin <sarasaadiq98@gmail.com>
2020-07-10 14:45:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,13 +2,13 @@ class Polygon {
   constructor(sides) {
     this.sides = sides
     this.accSides = this.sides.length
-    
+
   }
   get countSides(){
     return this.sides.length
   }
   get perimeter(){
-  return  if(!Array.isArray(this.sides)) 
+  return  if(!Array.isArray(this.sides))
     let sum = 0
     for (let i of this.sides) {
       sum = sum + i
